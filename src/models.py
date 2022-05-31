ce41b5b7b3727b886da99f17f83d96087e5fb815
Minimage <seancampbell914@gmail.com>
2022-05-31 12:54:11 +0000

    pushing to use git

--- src/models.py
+++ src/models.py
@@ -29,5 +29,37 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+    
+class People(Base):
+    __tablename__ = 'people'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(20))
+    hair_color = Column(String(20))
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+    person = relationship('Planet')
+    
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    
+
+
+class Enhabit(Base):
+    __tablename__ = 'enhabit'
+    id = Column(Integer, primary_key=True)
+    planet = Column(Integer, ForeignKey('planet.id'))
+
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
